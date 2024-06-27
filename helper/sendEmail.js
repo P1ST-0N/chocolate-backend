04f2065c72a8c3fd022867cc162c43d0e5a63d0b
P1ST-0N <luckylionia@gmail.com>
2024-06-27 16:55:38 +0300

    Update sendEmail.js

--- helper/sendEmail.js
+++ helper/sendEmail.js
@@ -1,1 +1,13 @@
-// import
+import nodemailer from "nodemailer";
+import "dotenv/config";
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+export const sendEmail = (message) => transport.sendEmail(message);
