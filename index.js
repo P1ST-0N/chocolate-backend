c7e8e0f831a8a82e3f681f0c7884246b6cf3458a
P1ST-0N <luckylionia@gmail.com>
2024-07-24 18:40:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,24 @@ const app = express();
 const { DB_HOST, PORT } = process.env;
 
 app.use(morgan("tiny"));
-app.use(cors());
+
+const allowedOrigins = [
+  "https://project-chocolate-market.vercel.app",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
